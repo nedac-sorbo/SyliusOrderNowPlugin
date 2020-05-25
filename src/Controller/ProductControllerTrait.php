659b55913fd4441bdf299385bb38995f8b114123
Youri Westerman <youri.westerman@nedac.eu>
2020-05-25 19:23:54 +0200

    Overriding templates and adding our own form theme for this form

--- src/Controller/ProductControllerTrait.php
+++ src/Controller/ProductControllerTrait.php
@@ -5,14 +5,18 @@ declare(strict_types=1);
 namespace Nedac\SyliusOrderNowPlugin\Controller;
 
 use FOS\RestBundle\View\View;
+use Pagerfanta\Pagerfanta;
+use Sylius\Bundle\CoreBundle\Form\Type\Order\AddToCartType;
 use Sylius\Bundle\ResourceBundle\Controller\EventDispatcherInterface;
 use Sylius\Bundle\ResourceBundle\Controller\RequestConfiguration;
 use Sylius\Bundle\ResourceBundle\Controller\RequestConfigurationFactoryInterface;
 use Sylius\Bundle\ResourceBundle\Controller\ResourcesCollectionProviderInterface;
 use Sylius\Bundle\ResourceBundle\Controller\ViewHandlerInterface;
+use Sylius\Bundle\ResourceBundle\Grid\View\ResourceGridView;
 use Sylius\Component\Resource\Metadata\MetadataInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Sylius\Component\Resource\ResourceActions;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -24,7 +28,7 @@ use Symfony\Component\HttpFoundation\Response;
  * @property EventDispatcherInterface $eventDispatcher
  * @property ViewHandlerInterface $viewHandler
  * @method void isGrantedOr403(RequestConfiguration $configuration, string $permission)
- *
+ * @method FormInterface createForm(string $type, $data = null, array $options = [])
  */
 trait ProductControllerTrait
 {
@@ -33,13 +37,27 @@ trait ProductControllerTrait
         $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
 
         $this->isGrantedOr403($configuration, ResourceActions::INDEX);
-        $resources = $this->resourcesCollectionProvider->get($configuration, $this->repository);
 
+        $resources = $this->resourcesCollectionProvider->get($configuration, $this->repository);
         $this->eventDispatcher->dispatchMultiple(ResourceActions::INDEX, $configuration, $resources);
 
         $view = View::create($resources);
 
         if ($configuration->isHtmlRequest()) {
+            $forms = [];
+
+            if ($resources instanceof ResourceGridView) {
+                /** @var Pagerfanta $pager */
+                $pager = $resources->getData();
+
+                foreach ($pager->getIterator() as $product) {
+                    $forms[] = $this
+                        ->createForm(AddToCartType::class, null, ['product' => $product])
+                        ->createView()
+                    ;
+                }
+            }
+
             $view
                 ->setTemplate($configuration->getTemplate(ResourceActions::INDEX . '.html'))
                 ->setTemplateVar($this->metadata->getPluralName())
@@ -48,6 +66,7 @@ trait ProductControllerTrait
                     'metadata' => $this->metadata,
                     'resources' => $resources,
                     $this->metadata->getPluralName() => $resources,
+                    'forms' => $forms
                 ])
             ;
         }
