1c94e7fd540bcc07f7ecb660a6e387ab50346d52
Youri Westerman <youri.westerman@nedac.eu>
2020-06-02 18:21:46 +0200

    Refactored test into one that goes on to the flasher path

--- tests/Unit/Controller/OrderItemControllerTest.php
+++ tests/Unit/Controller/OrderItemControllerTest.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Mockery;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use Nedac\SyliusOrderNowPlugin\Controller\OrderItemController;
+use Sylius\Bundle\OrderBundle\Controller\AddToCartCommandInterface;
 use Sylius\Bundle\ResourceBundle\Controller\AuthorizationCheckerInterface;
 use Sylius\Bundle\ResourceBundle\Controller\EventDispatcherInterface;
 use Sylius\Bundle\ResourceBundle\Controller\FlashHelperInterface;
@@ -23,10 +24,14 @@ use Sylius\Bundle\ResourceBundle\Controller\ResourceUpdateHandlerInterface;
 use Sylius\Bundle\ResourceBundle\Controller\SingleResourceProviderInterface;
 use Sylius\Bundle\ResourceBundle\Controller\StateMachineInterface;
 use Sylius\Bundle\ResourceBundle\Controller\ViewHandlerInterface;
+use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\Model\OrderItemInterface;
 use Sylius\Component\Resource\Factory\FactoryInterface;
 use Sylius\Component\Resource\Metadata\MetadataInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormErrorIterator;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -57,9 +62,19 @@ final class OrderItemControllerTest extends MockeryTestCase
         $this->assertInstanceOf(ResourceController::class, $controller);
     }
 
-    public function testRedirectsToReferer(): void
+    public function testAddsFormErrorFlasher(): void
     {
         $configuration = Mockery::mock(RequestConfiguration::class);
+        $configuration
+            ->shouldReceive('getFormType')
+            ->once()
+            ->andReturn('FORM_TYPE')
+        ;
+        $configuration
+            ->shouldReceive('getFormOptions')
+            ->once()
+            ->andReturn([])
+        ;
 
         $requestConfigurationFactory = Mockery::mock(RequestConfigurationFactoryInterface::class);
         $requestConfigurationFactory
@@ -78,12 +93,81 @@ final class OrderItemControllerTest extends MockeryTestCase
             ->andReturn($newResponse)
         ;
 
+        $newResource = Mockery::mock(OrderItemInterface::class);
+
+        $newResourceFactory = Mockery::mock(NewResourceFactoryInterface::class);
+        $newResourceFactory
+            ->shouldReceive('create')
+            ->once()
+            ->andReturn($newResource)
+        ;
+
+        $command = Mockery::mock(AddToCartCommandInterface::class);
+
+        $error = Mockery::mock(FormError::class);
+        $error
+            ->shouldReceive('getMessage')
+            ->once()
+            ->andReturn('big bad error message')
+        ;
+
+        $formErrorIterator = Mockery::mock(FormErrorIterator::class);
+        $formErrorIterator->shouldReceive('rewind');
+        $formErrorIterator
+            ->shouldReceive('valid')
+            ->twice()
+            ->andReturn(true, false)
+        ;
+        $formErrorIterator
+            ->shouldReceive('current')
+            ->once()
+            ->andReturn($error)
+        ;
+        $formErrorIterator->shouldReceive('next');
+
+        $form = Mockery::mock(FormInterface::class);
+        $form
+            ->shouldReceive('handleRequest->isValid')
+            ->once()
+            ->andReturn(false)
+        ;
+        $form
+            ->shouldReceive('getErrors')
+            ->once()
+            ->andReturn($formErrorIterator)
+        ;
+
         $controller = Mockery::mock(OrderItemController::class)->makePartial();
         $controller->shouldAllowMockingProtectedMethods();
-
         $controller
-            ->shouldReceive('getParentAddActionResponse')
-            ->andReturn(Mockery::mock(RedirectResponse::class))
+            ->shouldReceive('getCurrentCart')
+            ->once()
+            ->andReturn(Mockery::mock(OrderInterface::class))
+        ;
+        $controller
+            ->shouldReceive('isGrantedOr403')
+            ->once()
+        ;
+        $controller
+            ->shouldReceive('getQuantityModifier->modify')
+            ->with($newResource, 1)
+            ->once()
+        ;
+        $controller
+            ->shouldReceive('createAddToCartCommand')
+            ->once()
+            ->andReturn($command)
+        ;
+        $controller
+            ->shouldReceive('getFormFactory->create')
+            ->with('FORM_TYPE', $command, [])
+            ->once()
+            ->andReturn($form)
+        ;
+        $controller
+            ->shouldReceive('addFlash')
+            ->once()
+            ->with('error', 'big bad error message')
         ;
 
         $controller->__construct(
@@ -92,7 +176,7 @@ final class OrderItemControllerTest extends MockeryTestCase
             Mockery::mock(ViewHandlerInterface::class),
             Mockery::mock(RepositoryInterface::class),
             Mockery::mock(FactoryInterface::class),
-            Mockery::mock(NewResourceFactoryInterface::class),
+            $newResourceFactory,
             Mockery::mock(ObjectManager::class),
             Mockery::mock(SingleResourceProviderInterface::class),
             Mockery::mock(ResourcesCollectionProviderInterface::class),
@@ -107,6 +191,12 @@ final class OrderItemControllerTest extends MockeryTestCase
         );
 
         $request = Mockery::mock(Request::class);
+        $request
+            ->shouldReceive('isMethod')
+            ->with('POST')
+            ->once()
+            ->andReturn(true)
+        ;
 
         $this->assertSame($newResponse, $controller->addAction($request));
     }
