3e26af80f545f00e03829af50beea7aa5408e6a7
Youri Westerman <youri.westerman@nedac.eu>
2020-07-08 17:03:35 +0200

    Added context for clearing tables

---
+++ tests/Behat/Hook/ClearContext.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Nedac\SyliusOrderNowPlugin\Behat\Hook;
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Hook\Call\BeforeScenario;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+
+final class ClearContext implements Context
+{
+    private array $repositories = [];
+
+    public function __construct(RepositoryInterface ...$repositories) {
+        $this->repositories = $repositories;
+    }
+
+    /**
+     * @BeforeScenario
+     */
+    public function clearProducts(BeforeScenarioScope $scope): void
+    {
+        foreach ($this->repositories as $repository) {
+            $items = $repository->findAll();
+            foreach ($items as $item) {
+                $repository->remove($item);
+            }
+        }
+    }
+}
