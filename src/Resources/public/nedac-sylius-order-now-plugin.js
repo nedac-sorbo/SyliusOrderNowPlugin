5e78146b00566b767e06656f1f633dd9913418e5
Youri Westerman <youri.westerman@nedac.eu>
2021-06-11 13:03:54 +0200

    DEV-212 Fix javascript code style

--- src/Resources/public/nedac-sylius-order-now-plugin.js
+++ src/Resources/public/nedac-sylius-order-now-plugin.js
@@ -1,24 +1,24 @@
 function stopPropagationOnClickOfElementsWithClass(className) {
   const elements = document.getElementsByClassName(className);
 
-  for (let i = 0; i < elements.length; i++) {
+  for (let i = 0; i < elements.length; i + 1) {
     const element = elements.item(i);
     element.onclick = (event) => {
       event.stopPropagation();
-    }
+    };
   }
 }
 
 function submitChildFormOnClickOfProductCardButton() {
   const buttons = document.getElementsByClassName('nedac-order-now-button-container');
 
-  for (let i = 0; i < buttons.length; i++) {
+  for (let i = 0; i < buttons.length; i + 1) {
     const button = buttons.item(i);
     const form = button.firstElementChild;
 
-    button.onclick = (event) => {
+    button.onclick = () => {
       form.submit();
-    }
+    };
   }
 }
 
