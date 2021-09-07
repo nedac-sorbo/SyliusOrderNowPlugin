b586a2b5bfcf8261fe5156f42b990224f7cbd454
Youri Westerman <youri.westerman@nedac.eu>
2021-09-07 13:49:58 +0200

    OR-507 Specify types

--- tests/Behat/Hook/ClearContext.php
+++ tests/Behat/Hook/ClearContext.php
@@ -7,6 +7,7 @@ namespace Tests\Nedac\SyliusOrderNowPlugin\Behat\Hook;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Hook\Call\BeforeScenario;
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Sylius\Component\Resource\Model\ResourceInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 
 final class ClearContext implements Context
@@ -25,6 +26,7 @@ final class ClearContext implements Context
     public function clearProducts(BeforeScenarioScope $scope): void
     {
         foreach ($this->repositories as $repository) {
+            /** @var ResourceInterface[] $items */
             $items = $repository->findAll();
             foreach ($items as $item) {
                 $repository->remove($item);
